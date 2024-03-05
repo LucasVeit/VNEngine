a9585a8775dda54d409151b2b6a27042d0858bfd
Lucas <lucas-cs1998@hotmail.com>
2024-03-04 22:25:05 -0300

    setup for pip install

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vnengine',
+    version='0.1',
+    packages=find_packages(),
+    description='Library used for development of Visual Novels',
+    author='Lucas Veit',
+    author_email='',
+    url='',
+    test_suite='tests',
+    classifiers=[
+        'Programming Language :: Python :: 3.12',   
+    ],
+)
